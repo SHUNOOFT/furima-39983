2d606c34af78ac79a7b18877d79d251d20e17961
SHUNOOFT <146808272+SHUNOOFT@users.noreply.github.com>
2023-11-08 18:45:08 +0900

    テストコード修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,21 @@ RSpec.describe User, type: :model do
   end
 
   describe 'ユーザー新規登録' do
+    context '正常系テスト' do
+      it '全ての項目が正しい場合、登録できること' do
+        @user.nickname = 'テストユーザー'
+        @user.first_name = '太郎'
+        @user.last_name = '山田'
+        @user.first_name_kana = 'タロウ'
+        @user.last_name_kana = 'ヤマダ'
+        @user.birthday = '1990-01-01'
+        @user.email = 'test@example.com'
+        @user.password = 'Password123'
+        @user.password_confirmation = 'Password123'
+        expect(@user).to be_valid
+        expect { @user.save }.to change(User, :count).by(1)
+      end
+    end
 
     it 'nicknameが空では登録できない' do
       @user.nickname = ''
@@ -47,31 +62,42 @@ RSpec.describe User, type: :model do
       expect(another_user.errors.full_messages).to include('Email has already been taken')
     end
 
-    it 'お名前(全角)は、全角（漢字・ひらがな・カタカナ）での入力が必須であること' do
-      @user.last_name = '山田'
-      @user.first_name = '太郎'
-      expect(@user).to be_valid
+    it '名字が空欄の場合、エラーが表示されること' do
+      @user.last_name = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Last name can't be blank")
     end
-    it 'お名前(全角)は、全角（漢字・ひらがな・カタカナ）での入力が必須です' do
-      @user.last_name = '山田'
-      @user.first_name = '太郎'
-      expect(@user).to be_valid
+    it '名前が空欄の場合、エラーが表示されること' do
+      @user.first_name = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("First name can't be blank")
     end
-    it 'お名前（カナ）は、名字と名前がそれぞれ必須であること' do
-      @user.last_name_kana = 'ヤマダ'
-      @user.first_name_kana = 'タロウ'
-      expect(@user).to be_valid
+    it '名字（カナ）が空欄の場合、エラーが表示されること' do
+      @user.last_name_kana = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Last name kana can't be blank")
     end
-    it 'お名前（カナ）は、カタカナでの入力が必須であること' do
-      @user.last_name_kana = 'ヤマダ'
-      @user.first_name_kana = 'タロウ'
-      expect(@user).to be_valid
+    it '名前（カナ）が空欄の場合、エラーが表示されること' do
+      @user.first_name_kana = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("First name kana can't be blank")
     end
 
-    it 'パスワードは、半角英数字混合での入力が必須であること' do
-      # パスワードを設定
-      @user.password = 'password'
+    it '英字のみのパスワードでは登録できないこと' do
+      @user.password = 'password' # 英字のみのパスワード
       @user.password_confirmation = 'password'
+      expect(@user).not_to be_valid
+      expect(@user.errors.full_messages).to include('Password には英字と数字の両方を含めて設定してください')
+    end
+    it '数字のみのパスワードでは登録できないこと' do
+      @user.password = '123456' # 数字のみのパスワード
+      @user.password_confirmation = '123456'
+      expect(@user).not_to be_valid
+      expect(@user.errors.full_messages).to include('Password には英字と数字の両方を含めて設定してください')
+    end
+    it '数字のみのパスワードでは登録できないこと' do
+      @user.password = '123456' # 数字のみのパスワード
+      @user.password_confirmation = '123456'
       expect(@user).not_to be_valid
       expect(@user.errors.full_messages).to include('Password には英字と数字の両方を含めて設定してください')
     end
