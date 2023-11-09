88dda610226ff904c3e02df3c71fb55233f727e8
SHUNOOFT <146808272+SHUNOOFT@users.noreply.github.com>
2023-11-09 17:59:47 +0900

    全体修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,17 @@
 class Item < ApplicationRecord
-  validates :category_id, :condition_id,:shipping_fee_id,:shipping_city_id,:shipping_day_id, presence: true
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  has_one_attached :image
+  belongs_to :category_id
+  belongs_to :condition_id
+  belongs_to :charge_id
+  belongs_to :prefecture_id
+  belongs_to :shipping_date_id
+
+  validates :image, :title,:info, :category_id, :charge_id,:prefecture_id, presence: true
 
   #ジャンルの選択が「---」の時は保存できないようにする
-  validates :image_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :category_id, :condition_id :charge_id,:prefecture_id, :shipping_date_id, numericality: { other_than: 1 , message: "can't be blank"}
 
   belongs_to :user
   has_one :purchase
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :image
 end
