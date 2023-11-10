df95f080e43c1b2d752bb995201e424e89e006e3
SHUNOOFT <146808272+SHUNOOFT@users.noreply.github.com>
2023-11-11 00:52:16 +0900

    単体テストコードの作成

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品機能' do
+    it '商品出品が可能であること' do
+      expect(@item).to be_valid
+    end
+
+    it '画像がない場合、出品されないこと' do
+      @item.image = nil
+      expect(@item).not_to be_valid
+    end
+
+    it 'タイトルがない場合、出品されないこと' do
+      @item.title = nil
+      expect(@item).not_to be_valid
+    end
+
+    # 他にも必要なテストケースを記述する
+  end
 end
