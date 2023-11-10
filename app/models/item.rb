8b77ef11d3cd2ec71251b5dbe644df2af7a6c2e9
SHUNOOFT <146808272+SHUNOOFT@users.noreply.github.com>
2023-11-10 23:37:47 +0900

    修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,20 +15,6 @@ class Item < ApplicationRecord
   # ジャンルの選択が「---」の時は保存できないようにする
   validates :category_id, :condition_id, :prefecture_id, :shipping_fee_id, :shipping_day_id, :price, numericality: { other_than: 1, message: "can't be blank"}
 
-
-  before_save :calculate_fee_and_profit
   validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
-
-  def calculate_fee_and_profit
-    fee_rate = 0.1
-    profit_rate = 0.9
-    
-    # 計算ロジック
-    self.sales_fee = (price * fee_rate).floor
-    puts "sales_fee: #{sales_fee}"
-    self.sales_profit = (price * profit_rate).floor
-    puts "sales_profit: #{sales_profit}"
-  end
-  # sales_fee、sales_profitのゲッター、セッターメソッドを追加
-  attr_accessor :sales_fee, :sales_profit
+  
 end
