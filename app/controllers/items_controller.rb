73f79f31c004bece4bbb5dc7c0d191fb8957f995
SHUNOOFT <146808272+SHUNOOFT@users.noreply.github.com>
2023-11-10 23:34:49 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,5 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
-  before_action :calculate_fee_and_profit, only: [:create, :update]
 
   def index
   end
@@ -22,10 +21,5 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:image, :title, :info, :category_id, :condition_id, :prefecture_id, :shipping_fee_id, :shipping_day_id, :price).merge(user_id: current_user.id)
-  end
-
-  def calculate_fee_and_profit
-    # items コントローラーにモデルの計算ロジックを呼び出す処理を書く
-    @item.calculate_fee_and_profit if @item
   end
 end
