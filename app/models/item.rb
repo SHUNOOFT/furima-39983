3ba1f44b576727d58a6000fc302a5e84e0fae55c
SHUNOOFT <146808272+SHUNOOFT@users.noreply.github.com>
2023-11-11 15:24:44 +0900

    販売手数料と販売利益の表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,8 +13,8 @@ class Item < ApplicationRecord
   validates :image, :title, :info, :category_id, :condition_id, :prefecture_id, :shipping_fee_id, :shipping_day_id, :price, presence: true
 
   # ジャンルの選択が「---」の時は保存できないようにする
-  validates :category_id, :condition_id, :prefecture_id, :shipping_fee_id, :shipping_day_id, :price, numericality: { other_than: 1, message: "can't be blank"}
+  validates :category_id, :condition_id, :prefecture_id, :shipping_fee_id, :shipping_day_id, numericality: { other_than: 1, message: "can't be blank"}
 
-  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   
 end
