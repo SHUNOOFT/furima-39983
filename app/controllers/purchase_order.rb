73b904836b7857cc4687b79c2151fbcdcd2640ff
SHUNOOFT <146808272+SHUNOOFT@users.noreply.github.com>
2023-11-15 18:20:46 +0900

    修正

--- app/controllers/purchase_order.rb
+++ app/controllers/purchase_order.rb
@@ -4,6 +4,9 @@ class PurchaseOrder < ApplicationRecord
   def index
   end
 
+  def create
+  end
+
   private
   def item_params
     params.require(:purchase_order).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
   resources :items 
-  root to: 'purchase_orders#index'
   resources :purchase_orders 
 
 end
