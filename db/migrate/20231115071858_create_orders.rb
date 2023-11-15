80542d7110f8fa339f414f0c21e665b7be88086a
SHUNOOFT <146808272+SHUNOOFT@users.noreply.github.com>
2023-11-15 22:48:30 +0900

    マイグレーション実行

---
+++ db/migrate/20231115071858_create_orders.rb
@@ -0,0 +1,13 @@
+class CreateOrders < ActiveRecord::Migration[7.0]
+  def change
+    create_table :orders do |t|
+      t.string     :postcode,      null: false
+      t.integer    :prefecture_id, null: false
+      t.string     :city,          null: false
+      t.string     :block,         null: false
+      t.string     :building,      null: false
+      t.string     :phone_number,  null: false
+      t.timestamps
+    end
+  end
+end
