c1b1b3c7ef61fe38dbe5d30d73180b79f660b885
SHUNOOFT <146808272+SHUNOOFT@users.noreply.github.com>
2023-11-17 19:21:12 +0900

    手順6

--- spec/factories/purchase_orders.rb
+++ spec/factories/purchase_orders.rb
@@ -1,5 +1,21 @@
 FactoryBot.define do
   factory :purchase_order do
-    
+    postcode { '123-4567' }
+    prefecture_id { 1 }
+    city { 'Example City' }
+    block { 'Example Block' }
+    building { 'Example Building' }
+    phone_number { '09012345678' }
+    user_id { 1 }
+    item_id { 1 }
+
+
+    trait :without_postcode do
+      postcode { nil }
+    end
+
+    trait :without_prefecture_id do
+      prefecture_id { nil }
+    end
   end
 end
--- spec/models/purchase_order_spec.rb
+++ spec/models/purchase_order_spec.rb
@@ -1,5 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe PurchaseOrder, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '購入機能' do
+    before do
+      @purchase_order = FactoryBot.build(:purchase_order)
+    end
+
+    it '購入が可能であること' do
+      expect(@purchase_order).to be_valid
+    end
+
+    context '購入ができない場合' do
+      it '郵便番号が未入力の場合、購入できないこと' do
+        @purchase_order.postcode = nil
+        @purchase_order.valid?
+        expect(@purchase_order.errors.full_messages).to include("Postcode can't be blank")
+      end
+
+      it '都道府県が未選択の場合、購入できないこと' do
+        purchase_order = PurchaseOrder.new(prefecture_id: '')
+        purchase_order.valid?
+        expect(purchase_order.errors.full_messages).to include("Prefecture can't be blank")
+      end
+
+      it '市区町村が未入力の場合、購入できないこと' do
+        @purchase_order.city = nil
+        @purchase_order.valid?
+        expect(@purchase_order.errors.full_messages).to include("City can't be blank")
+      end
+
+      it '番地が未入力の場合、購入できないこと' do
+        @purchase_order.block = nil
+        @purchase_order.valid?
+        expect(@purchase_order.errors.full_messages).to include("Block can't be blank")
+      end
+
+      it '電話番号が未入力の場合、購入できないこと' do
+        @purchase_order.phone_number = nil
+        @purchase_order.valid?
+        expect(@purchase_order.errors.full_messages).to include("Phone number can't be blank")
+      end
+    end
+  end
 end
